cab1bbb04e45e0b0afd0e4d6f58d993239de34ea
Maxim Chukavov <maximchukavovmc@gmail.com>
2023-07-30 20:45:49 +0300

    added two funcions

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from aiogram import Bot, Dispatcher, executor, types
-
+import requests
+import logging
 from config import TOKEN_API
+from keyboard import keyboard
 
+
+logging.basicConfig(level=logging.INFO)
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
@@ -16,17 +20,29 @@ async def on_startup(_):
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer(text="Привет! Этот бот скачивает книги с сайте 'flibusta.club'. Для скачивания"
+    user = message.from_user
+    await message.answer(text=f"Привет, {user.first_name}! Этот бот скачивает книги с сайте 'flibusta.club'. Для скачивания"
                               " напиши имя книги или интересующего автора ")
     await message.delete()
 
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.answer(text=HELP_COMMAND)
+    await message.answer(text=HELP_COMMAND, reply_markup=keyboard)
     await message.delete()
 
 
+@dp.message_handler(content_types=types.ContentTypes.TEXT)
+async def search_book(message: types.Message):
+    pass
+
+
+async def search_book_on_flibusta(book_name: str, page: int):
+    if requests.status_codes == 200:
+        params = {'ask': book_name}
+        response = requests.get("https://flibusta.club/booksearch", params=params)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
 
